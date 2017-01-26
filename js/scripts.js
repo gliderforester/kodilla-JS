0b39c30a3fa22c806c279ad881760cda46420648
gliderforester <a.dolecki@gmail.com>
2017-01-26 23:31:10 +0100

    triangleArea

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 // plik scripts.js
+var a = prompt('Podaj długość podstawy trójkąta w cm');
+var h = prompt('Podaj wysokość trójkąta w cm');
+
+var triangleArea = .5 * a * h;
+
+console.log('Pole trójkąta wynosi: ' + triangleArea + 'cm');
+alert('Pole trójkąta wynosi: ' + triangleArea + 'cm');
 
