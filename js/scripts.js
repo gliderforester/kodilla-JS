f54cdb7f5b9db2992dca041769478d84a2bfeca5
gliderforester <a.dolecki@gmail.com>
2017-01-28 19:24:13 +0100

    function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
 // plik scripts.js
 
+function getTriangleArea(a, h) {
+  if ((a <= 0) || (h <=0)) {
+    return 'Nieprawidłowe dane';
+  } else {
+    return a * h/2;
+      }
+}
+var triangle1Area = getTriangleArea(14,6);
+var triangle2Area = getTriangleArea(7,8);
+var triangle3Area = getTriangleArea(0,34);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
+
+
+
