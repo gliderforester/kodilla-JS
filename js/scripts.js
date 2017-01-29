d1cabbd2df5e43b1977903f59dd56bdcdb77d7a0
gliderforester <a.dolecki@gmail.com>
2017-01-29 21:16:57 +0100

    tables joining

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 // plik scripts.js
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
+var allNames = femaleNames.concat(maleNames);
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+  var addedName = allNames.push(newName);
+} else {
+      console.log('To imię jest już w tabeli');
+}
+
+console.log(allNames);
