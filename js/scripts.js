62fbedf067862f00004880bac5e3dccebd7444ee
gliderforester <a.dolecki@gmail.com>
2017-01-31 21:19:26 +0100

    string

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 // plik scripts.js
 
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var textAfter = text.replace('Papugi', animal.toUpperCase());
+
+console.log(textAfter.slice(0, (textAfter.length/2)));
