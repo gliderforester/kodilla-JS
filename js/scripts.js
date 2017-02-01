d4f4fe7a5f4f8e8fe1550faf062c81ad0d31b88a
gliderforester <a.dolecki@gmail.com>
2017-02-01 01:39:13 +0100

    tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
-// plik scripts.js
+function rysujChoinke(lev) {
+  for (var n = 1; n <= lev; n++) {
+    var star = '';
+    var spac = '';
 
+    for (var l = 1; l <= n * 2 - 1; l++) {
+      star += '*';
+    }
+    for (var s = 0; s < lev-n; s++) {
+      spac += ' '
+    }
+  console.log(spac + star);
+  }
+
+
+
+}
