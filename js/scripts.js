69871fe1ce76dfed7190a03420cf178f6cb0e9f3
gliderforester <a.dolecki@gmail.com>
2017-02-01 16:32:05 +0100

    DOM-buttons with function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 // plik scripts.js
 
-var buttonName = document.getElementsByClassName('button');
-for (var n = 0; n < buttonName.length; n++) {
- alert(buttonName[n].innerText);
+var getButtons = document.getElementsByClassName('button');
+for (var n = 0; n < getButtons.length; n++) {
+  function getButtonName(n) {
+    return (getButtons[n].innerText);
+  }
+  console.log(getButtonName(n));
+  alert(getButtonName(n));
 }
