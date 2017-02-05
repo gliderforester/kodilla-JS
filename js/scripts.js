f5b59bb853f32d157a8dfa17d17279bcd2975a0c
gliderforester <a.dolecki@gmail.com>
2017-02-05 01:46:05 +0100

    lista

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 // plik scripts.js
+var getBtn = document.getElementById('addElem'),
+  getLst = document.getElementById('Lista'),
+  liNumber = document.getElementsByTagName('li');
+
+getBtn.addEventListener('click', function () {
+  getLst.innerHTML += '<li>item ' +liNumber.length+'</li>';
+
+});
+
 
